2777cabdeb2a19b9c3a9808753d49fae2dcb6700
thang109 <thien.thangg03@gmail.com>
2025-02-25 18:28:53 +0700

    add annotation candidate

--- WorkSmart.Application/Services/CandidateService.cs
+++ WorkSmart.Application/Services/CandidateService.cs
@@ -52,6 +52,8 @@ namespace WorkSmart.Application.Services
             if (request.Address != null) user.Address = request.Address;
             if (request.Avatar != null) user.Avatar = request.Avatar;
             if (request.DateOfBirth != null) user.DateOfBirth = request.DateOfBirth;
+            if (request.IdentityNumber != null) user.IdentityNumber = request.IdentityNumber;
+            if (request.IsPrivated != null) user.IsPrivated = (bool)request.IsPrivated;
 
             user.UpdatedAt = DateTime.UtcNow;
             _candidateRepository.Update(user);

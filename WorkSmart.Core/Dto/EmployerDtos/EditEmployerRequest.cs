78601097e8a2f65b3f108da8bd4747b191787b33
thang109 <thien.thangg03@gmail.com>
2025-02-28 14:07:25 +0700

    update edit candidate profile

--- WorkSmart.Core/Dto/EmployerDtos/EditEmployerRequest.cs
+++ WorkSmart.Core/Dto/EmployerDtos/EditEmployerRequest.cs
@@ -22,8 +22,11 @@ namespace WorkSmart.Core.Dto.EmployerDtos
 
         [MinLength(10, ErrorMessage = "Company Description must be at least 10 characters.")]
         public string? CompanyDescription { get; set; }
+        
         public DateTime? CreatedAt { get; set; }
+        
         public string? Avatar { get; set; }
+        
         public bool? IsPrivated { get; set; }
     }
 }

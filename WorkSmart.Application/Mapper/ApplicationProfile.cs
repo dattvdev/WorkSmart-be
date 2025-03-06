7a067b7658201dc07af31e375760e787232046e8
Thanh Truong Tuan <117159476+tuanthanh200702@users.noreply.github.com>
2025-03-06 23:20:21 +0700

    commit application profile

---
+++ WorkSmart.Application/Mapper/ApplicationProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkSmart.Application.Mapper
+{
+    public class ApplicationProfile : Profile
+    {
+        public ApplicationProfile()
+        {
+            CreateMap<Application, ApplicationJobDto>();
+        }
+    }
+}

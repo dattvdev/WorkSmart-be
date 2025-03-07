86311525215d1362029fe38a212c401247f47227
dattvdev <dattvdev@gmail.com>
2025-03-07 22:30:29 +0700

    subscription

---
+++ WorkSmart.Application/Services/SubscriptionService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkSmart.Core.Interface;
+
+namespace WorkSmart.Application.Services
+{
+    public class SubscriptionService
+    {
+        private readonly ISubscriptionRepository _subscriptionRepository;
+        public SubscriptionService(ISubscriptionRepository subscriptionRepository)
+        {
+            _subscriptionRepository = subscriptionRepository;
+        }
+
+    }
+}

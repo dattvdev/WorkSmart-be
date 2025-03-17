04b2835a4924f2986084a545fbf226ee0fe2ec17
dattvdev <dattvdev@gmail.com>
2025-03-17 19:58:08 +0700

    add property

--- WorkSmart.Core/Entity/Job.cs
+++ WorkSmart.Core/Entity/Job.cs
@@ -31,6 +31,8 @@ namespace WorkSmart.Core.Entity
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
         public string? ReasonRejectedJob { get; set; }
+        public int CategoryID { get; set; }
+
         public User User { get; set; }
         public ICollection<Application>? Applications { get; set; }
         public ICollection<FavoriteJob>? FavoriteJobs { get; set; }

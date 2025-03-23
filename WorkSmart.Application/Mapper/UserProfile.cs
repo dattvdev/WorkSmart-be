3c99ec636bed98a59a4362fc77907f1e5dcae5e2
dattvdev <dattvdev@gmail.com>
2025-03-23 21:13:24 +0700

    Update API Company Job

--- WorkSmart.Application/Mapper/UserProfile.cs
+++ WorkSmart.Application/Mapper/UserProfile.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorkSmart.Core.Dto.EmployerDtos;
+using WorkSmart.Core.Dto.JobDtos;
 using WorkSmart.Core.Dto.UserDtos;
 using WorkSmart.Core.Entity;
 
@@ -14,6 +16,9 @@ namespace WorkSmart.Application.Mapper
         public UserProfile()
         {
             CreateMap<User, UserMessageInfoDto>().ReverseMap();
+            CreateMap<Job, CompanyJobDto>().ReverseMap();
+            CreateMap<User, CompanyDto>()
+               .ForMember(dest => dest.PostedJobs, opt => opt.MapFrom(src => src.PostedJobs));
         }
     }
 }

506d02f1b1f6d5ef9cda09103b627fe3da858deb
trngchnii <ni.trnh59@gmail.com>
2025-03-26 22:02:35 +0700

    "change"

---
+++ WorkSmart.Application/Services/FavoriteJobService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using WorkSmart.Core.Interface;
+
+namespace WorkSmart.Application.Services
+{
+    public class FavoriteJobService
+    {
+        private readonly IFavoriteJobRepository _favoriteJobRepository;
+        private readonly IMapper _mapper;
+
+        public FavoriteJobService(IFavoriteJobRepository favoriteJobRepository
+            ,IMapper mapper)
+        {
+            _favoriteJobRepository = favoriteJobRepository;
+            _mapper = mapper;
+        }
+
+        public 
+    }
+}
---
+++ WorkSmart.Repository/Repository/FavoriteJobRepository.cs
@@ -0,0 +1,13 @@
+﻿using WorkSmart.Core.Entity;
+using WorkSmart.Core.Interface;
+
+namespace WorkSmart.Repository.Repository
+{
+    public class FavoriteJobRepository : BaseRepository<FavoriteJob>, IFavoriteJobRepository
+    {
+        public FavoriteJobRepository(WorksmartDBContext context) : base(context)
+        {
+
+        }
+    }
+}

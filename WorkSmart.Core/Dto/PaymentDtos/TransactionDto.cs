0a5da41e10b56eb7d80a3a53fd05e4965a7e4d94
thang109 <thien.thangg03@gmail.com>
2025-03-28 18:52:03 +0700

    update payment

---
+++ WorkSmart.Core/Dto/PaymentDtos/TransactionDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkSmart.Core.Dto.PaymentDtos
+{
+    public class TransactionDto
+    {
+        public long OrderCode { get; set; }
+        public double Price { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string UserFullName { get; set; }
+        public string UserEmail { get; set; }
+        // Chỉ chứa các thuộc tính cần thiết
+    }
+}

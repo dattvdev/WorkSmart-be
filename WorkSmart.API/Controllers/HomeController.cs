cc46d9be0a7c97a922bd503815d24cfaa89125d1
dattvdev <dattvdev@gmail.com>
2025-04-20 16:59:40 +0700

    test

--- WorkSmart.API/Controllers/HomeController.cs
+++ WorkSmart.API/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace WorkSmart.API.Controllers
 {
@@ -13,8 +14,10 @@ namespace WorkSmart.API.Controllers
         public IActionResult GetRandomNumber()
         {
             var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            var connectionString1 = Environment.GetEnvironmentVariable("CUSTOMCONNSTR_DefaultConnection"); // lấy từ tab "Connection strings" trong Azure
+            var connectionString2 = _configuration["ConnectionStrings:DefaultConnection"];
             var key = _configuration["Cohere:Key"];
-            return Ok(new List<string> { connectionString, key });
+            return Ok(new List<string> { connectionString, connectionString1, connectionString2, key });
         }
     }
 }

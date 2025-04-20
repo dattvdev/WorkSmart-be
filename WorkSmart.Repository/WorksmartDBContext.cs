94ec56cc220d53c2da8a3065d36d5ac987fe699a
Trần Văn Đạt <152597701+dattvdev@users.noreply.github.com>
2025-04-20 17:18:37 +0700

    Update WorksmartDBContext.cs

--- WorkSmart.Repository/WorksmartDBContext.cs
+++ WorkSmart.Repository/WorksmartDBContext.cs
@@ -38,11 +38,11 @@ namespace WorkSmart.Repository
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var builder = new ConfigurationBuilder()
-               .SetBasePath(Directory.GetCurrentDirectory())
-               .AddJsonFile("appsettings.json", true, true);
-            IConfigurationRoot config = builder.Build();
-            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            // var builder = new ConfigurationBuilder()
+            //    .SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json", true, true);
+            // IConfigurationRoot config = builder.Build();
+            // optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

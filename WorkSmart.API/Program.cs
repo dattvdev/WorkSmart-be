72fa47a3147c31e1ce7cb6653de16e7c42734808
Trần Văn Đạt <152597701+dattvdev@users.noreply.github.com>
2025-04-20 13:21:19 +0700

    Update Program.cs Connect DB

--- WorkSmart.API/Program.cs
+++ WorkSmart.API/Program.cs
@@ -32,7 +32,7 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddControllers();
 
-builder.Services.AddScopeCollection(builder.Configuration.GetConnectionString("DefaultConnection").ToString());
+builder.Services.AddScopeCollection(builder.Configuration["ConnectionStrings:DefaultConnection"]);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

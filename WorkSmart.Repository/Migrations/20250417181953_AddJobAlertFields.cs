f8c03bae998ec3d0a760f0211495117869b08978
trngchnii <ni.trnh59@gmail.com>
2025-04-23 19:11:11 +0700

    add update package

---
+++ WorkSmart.Repository/Migrations/20250417181953_AddJobAlertFields.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkSmart.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddJobAlertFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WorkType",
+                table: "JobAlerts",
+                newName: "SalaryRange");
+
+            migrationBuilder.RenameColumn(
+                name: "SalaryLevel",
+                table: "JobAlerts",
+                newName: "Province");
+
+            migrationBuilder.RenameColumn(
+                name: "NotifyBy",
+                table: "JobAlerts",
+                newName: "NotificationMethod");
+
+            migrationBuilder.RenameColumn(
+                name: "City",
+                table: "JobAlerts",
+                newName: "JobType");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SalaryRange",
+                table: "JobAlerts",
+                newName: "WorkType");
+
+            migrationBuilder.RenameColumn(
+                name: "Province",
+                table: "JobAlerts",
+                newName: "SalaryLevel");
+
+            migrationBuilder.RenameColumn(
+                name: "NotificationMethod",
+                table: "JobAlerts",
+                newName: "NotifyBy");
+
+            migrationBuilder.RenameColumn(
+                name: "JobType",
+                table: "JobAlerts",
+                newName: "City");
+        }
+    }
+}
